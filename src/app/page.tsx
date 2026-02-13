a78a9fc4d0af334403fa224446a8706687a971c9
syedshafinahmed <shafinahmed.cse@gmail.com>
2026-02-13 12:49:41 +0600

    Added Favicon & Font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p>hehei bhaya</p>
+    <section className="bg-background text-text py-20">
+      <h1 className="text-4xl font-bold text-primary">
+        Learn Smarter, Learn Better
+      </h1>
+
+      <p className="mt-4 max-w-xl">
+        Personalized tuition for real academic growth.
+      </p>
+
+      <button className="mt-6 bg-accent text-text px-6 py-3 rounded-lg font-semibold transition">
+        Enroll Now
+      </button>
+    </section>
   );
 }
